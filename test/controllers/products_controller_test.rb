e338b1d343a6ad742e4a205e0ed290ed28bbcc2e
geckoplus <pavlifestyle@gmail.com>
2014-03-28 10:49:03 +0100

    Add Carts and play with Layouts

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = { :title => 'Lorem ipsum', :description => 'Wibbles are fun!', :image_url => 'lorem.jpg', :price => 19.95}
   end
 
   test "should get index" do
@@ -16,6 +17,7 @@ class ProductsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+=begin
   test "should create product" do
     assert_difference('Product.count') do
       post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
@@ -23,6 +25,7 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to product_path(assigns(:product))
   end
+=end
 
   test "should show product" do
     get :show, id: @product
@@ -33,11 +36,12 @@ class ProductsControllerTest < ActionController::TestCase
     get :edit, id: @product
     assert_response :success
   end
-
+=begin
   test "should update product" do
     patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     assert_redirected_to product_path(assigns(:product))
   end
+=end  
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
@@ -46,4 +50,31 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
+  #... moje
+
+    test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:product)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create product" do
+    assert_difference('Product.count') do
+      post :create, :product => @update
+    end
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  # ...
+  test "should update product" do
+    put :update, :id => @product.to_param, :product => @update
+    assert_redirected_to product_path(assigns(:product))
+  end
+  # ...
 end
